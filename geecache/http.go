dd327d5cba1f0d8cc0fa2ac463e2cc14dc68fd4c
fenghaizhi <68504564+FaithThirteen@users.noreply.github.com>
2021-10-21 14:49:51 +0800

    http获取缓存注释

--- geecache/http.go
+++ geecache/http.go
@@ -30,20 +30,20 @@ func (p *HTTPPool) Log(format string, v ...interface{}) {
 	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
 }
 
-// ServeHTTP handle all http requests
+// ServeHTTP 实现http获取缓存
 func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if !strings.HasPrefix(r.URL.Path, p.basePath) {
 		panic("HTTPPool serving unexpected path: " + r.URL.Path)
 	}
 	p.Log("%s %s", r.Method, r.URL.Path)
-	// /<basepath>/<groupname>/<key> required
+	// /<basepath>/<groupname>/<key> 约定的规范请求路径
 	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
 	if len(parts) != 2 {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
 
-	// 约定的路径获取group与key
+	// 获取group与key
 	groupName := parts[0]
 	key := parts[1]
 
