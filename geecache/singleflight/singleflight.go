d234ba04edf70d7ad4daa6f2b5a1f3cb498d5cce
fenghaizhi <68504564+FaithThirteen@users.noreply.github.com>
2021-10-26 11:34:52 +0800

    feat:防止缓存击穿注释

--- geecache/singleflight/singleflight.go
+++ geecache/singleflight/singleflight.go
@@ -15,13 +15,14 @@ type Group struct {
 }
 
 // Do 并发时无论 Do 被调用多少次，函数 fn 都只会被调用一次
+// https://pkg.go.dev/golang.org/x/sync/singleflight 标准库的singleflight实现
 func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
-	// 加锁防止进入时添加
+	// 加锁防止进入时其他请求添加key
 	g.mu.Lock()
 	if g.m == nil {
 		g.m = make(map[string]*call)
 	}
-	// map中含有说明请求进行中，需要解锁等待执行完成然后取上个g填入的值
+	// map中含有说明请求进行中，需要解锁等待执行完成然后取上个请求填入的值
 	if c, ok := g.m[key]; ok {
 		g.mu.Unlock()
 		c.wg.Wait()
@@ -29,6 +30,7 @@ func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, err
 	}
 
 	// 执行中，添加到 g.m，表明 key 已经有对应的请求在处理
+	// 通过sync.WaitGroup实现让其他请求等待返回
 	c := new(call)
 	c.wg.Add(1)
 	g.m[key] = c
