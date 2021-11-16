ff9fc925c8fd980ebd03dec4a76aa4d6652f9e85
fenghaizhi <68504564+FaithThirteen@users.noreply.github.com>
2021-11-16 20:01:17 +0800

    feat:添加服务端与消息解码

--- geerpc/server.go
+++ geerpc/server.go
@@ -1,17 +1,148 @@
 package geerpc
 
-
-import "geerpc/codec"
+import (
+	"encoding/json"
+	"fmt"
+	"geerpc/codec"
+	"io"
+	"log"
+	"net"
+	"reflect"
+	"sync"
+)
 
 const MagicNumber = 0x3bef5c
 
 type Option struct {
-	MagicNumber int        // MagicNumber marks this's a geerpc request
-	CodecType   codec.Type // 客户端可以选择不同的编码器进行编码
+	MagicNumber int        // 标记这是grpc请求
+	CodecType   codec.Type // 编解码类型
 }
 
-var DefaultOption = &Option{
+var DefaultOption = Option{
 	MagicNumber: MagicNumber,
 	CodecType:   codec.GobType,
 }
 
+// Server 是一个rpc服务器
+type Server struct {
+}
+
+// NewServer 返回一个Server
+func NewServer() *Server {
+	return &Server{}
+}
+
+// DefaultServer 返回一个默认的server
+var DefaultServer = NewServer()
+
+func (server *Server) Accept(lis net.Listener) {
+
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error::", err)
+			return
+		}
+		go server.ServeConn(conn)
+	}
+}
+
+// Accept 接受侦听器上的连接并提供请求
+// 对于每个传入连接。
+func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
+
+// ServeConn 在单个连接上运行服务器。
+// ServeConn 阻塞，服务连接直到客户端挂断。
+func (server *Server) ServeConn(conn io.ReadWriteCloser) {
+	defer func() { _ = conn.Close() }()
+
+	var option Option
+	if err := json.NewDecoder(conn).Decode(&option); err != nil {
+		log.Println("rpc server: options error: ", err)
+		return
+	}
+	if option.MagicNumber != MagicNumber {
+		log.Printf("rpc server: invalid magic number %x", option.MagicNumber)
+		return
+	}
+
+	f := codec.NewCodecFuncMap[option.CodecType]
+
+	if f == nil {
+		log.Printf("rpc server: invalid codec type %s", option.CodecType)
+		return
+	}
+
+	server.serveCodec(f(conn))
+}
+
+var invalidRequest = struct{}{}
+
+func (server *Server) serveCodec(cc codec.Codec) {
+
+	sending := new(sync.Mutex) // 确保发送完整的响应
+	wg := new(sync.WaitGroup)  // 等待所有请求处理完毕
+	for {
+		req, err := server.readRequest(cc)
+		if err != nil {
+			if req == nil {
+				break // it's not possible to recover, so close the connection
+			}
+			req.h.Error = err.Error()
+			server.sendResponse(cc, req.h, invalidRequest, sending)
+			continue
+		}
+		wg.Add(1)
+		go server.handleRequest(cc, req, sending, wg)
+	}
+	wg.Wait()
+	_ = cc.Close()
+}
+
+type request struct {
+	h            *codec.Header // 请求头
+	argv, replyv reflect.Value // 请求参数和返回
+}
+
+func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
+	var h codec.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error:", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+
+func (server *Server) readRequest(cc codec.Codec) (*request, error) {
+	h, err := server.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{h: h}
+	// TODO: now we don't know the type of request argv
+	// day 1, just suppose it's string
+	req.argv = reflect.New(reflect.TypeOf(""))
+	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+		log.Println("rpc server: read argv err:", err)
+	}
+	return req, nil
+}
+
+func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
+	sending.Lock()
+	defer sending.Unlock()
+	if err := cc.Write(h, body); err != nil {
+		log.Println("rpc server: write response error:", err)
+	}
+}
+
+func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+	// TODO, should call registered rpc methods to get the right replyv
+	// day 1, just print argv and send a hello message
+	defer wg.Done()
+	log.Println(req.h, req.argv.Elem())
+	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq))
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+}
